ee479ccf8df6aa50261f4cc348c58d60026dac7d
Arthur <art.jacintho14@gmail.com>
2025-10-17 09:51:44 -0300

    first commit

---
+++ src/main/java/com/biblioteca/BibliotecaBD/BibliotecaBdApplication.java
@@ -0,0 +1,13 @@
+package com.biblioteca.BibliotecaBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBdApplication.class, args);
+	}
+
+}
